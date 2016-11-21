a0553c3c921579f08a907f8ace55466973b950a9
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-11-21 12:47:08 +0100

    userman: refactor API

--- modules/userman.php
+++ modules/userman.php
@@ -2,14 +2,19 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-function __user($username) {
-    exec('/usr/bin/hostname -d', $out, $ret);
-    $domain = $out[0];
-    return "$username@$domain";
+function getUser($username) {
+
+    # add domain part if needed
+    if (strpos($username, '@') === false) {
+        exec('/usr/bin/hostname -d', $out, $ret);
+        $domain = $out[0];
+        return "$username@$domain";
+    }
+    return $username;
 }
 
-function __exists($username) {
-    exec("/usr/bin/getent passwd '".__user($username)."'", $out, $ret);
+function userExists($username) {
+    exec("/usr/bin/getent passwd '".getUser($username)."'", $out, $ret);
     return ($ret === 0);
 }
 
@@ -19,46 +24,73 @@ $app->get('/users', function (Request $request, Response $response, $args) {
 });
 
 
-$app->get('/users/{id:[0-9]+}', function (Request $request, Response $response, $args) {
-    $route = $request->getAttribute('route');
-    $id = $route->getArgument('id');
-    $users = FreePBX::create()->Userman->getAllUsers();
-    foreach ($users as $u) {
-        if ($u['id'] == $id){
-            return $response->withJson($u);
-        }
-    }
-    return $response->withJson(array());
-});
-
-
-$app->get('/users/exists/{username}', function (Request $request, Response $response, $args) {
-print_r($params);
+$app->get('/users/{username}', function (Request $request, Response $response, $args) {
     $username = $request->getAttribute('username');
-    return $response->withJson(['result' => __exists($username)]);
-});
-
-$app->put('/users/create/{username}/{fullname}/{extension}', function (Request $request, Response $response, $args) {
-    $username = $request->getAttribute('username');
-    $fullname = $request->getAttribute('fullname');
-    exec("/usr/bin/sudo /sbin/e-smith/signal-event user-create '$username' '$fullname' '/bin/false'", $out, $ret);
-    if ( $ret === 0 ) {
-        return $response->withJson(['result' => true]);
+    if (userExists($username)) {
+        return $response->withJson(['result' => userExists($username)], 200);
+    } else {
+        return $response->withJson(['result' => 'Not found'], 404);
     }
-    return $response->withJson(['result' => false]);
 });
 
-$app->post('/users/setpassword', function (Request $request, Response $response, $args) {
+
+# Create or edit a system user inside OpenLDAP
+# Should be used only in legacy mode.
+#
+# JSON body:
+#
+# {"username" : "myuser", "fullname" : "my full name"}
+
+
+$app->post('/users', function (Request $request, Response $response, $args) {
     $params = $request->getParsedBody();
     $username = $params['username'];
+    $fullname = $params['fullname'];
+    if ( ! $username || ! $fullname ) {
+        return $response->withJson(['result' => 'User name or full name invalid'], 422);
+    }
+    if ( userExists($username) ) {
+        exec("/usr/bin/sudo /sbin/e-smith/signal-event user-modify '$username' '$fullname' '/bin/false'", $out, $ret);
+    } else {
+        exec("/usr/bin/sudo /sbin/e-smith/signal-event user-create '$username' '$fullname' '/bin/false'", $out, $ret);
+    }
+    if ( $ret === 0 ) {
+        return $response->withJson(['result' => true], 201);
+    } else {
+        return $response->withJson(['result' => false], 422);
+    }
+});
+
+
+# Set the password of a given user
+# Should be used only in legacy mode.
+#
+# JSON body:
+#
+# {"password" : "mypassword"}
+
+$app->post('/users/{username}/password', function (Request $request, Response $response, $args) {
+    $params = $request->getParsedBody();
+    $username = $request->getAttribute('username');
     $password = $params['password'];
-    if (__exists($username)) {
+    if ( ! userExists($username) ) {
+        return $response->withJson(['result' => "$username user doesn't exist"], 422);
+    } else {
         $tmp = tempnam("/tmp","ASTPWD");
         file_put_contents($tmp, $password);
 
-        exec("/usr/bin/sudo /sbin/e-smith/signal-event password-modify '".__user($username)."' $tmp", $out, $ret);
+        exec("/usr/bin/sudo /sbin/e-smith/signal-event password-modify '".getUser($username)."' $tmp", $out, $ret);
         return $response->withJson(['result' => ($ret === 0)]);
     }
-    return $response->withJson(['result' => false]);
+    return $response->withJson(['result' => false], 422);
 }); 
+
+# Return the password givent user in clear text
+# Should be used only in legacy mode.
+
+$app->get('/users/{username}/password', function (Request $request, Response $response, $args) {
+    $params = $request->getParsedBody();
+    $username = $request->getAttribute('username');
+    return $response->withJson(['result' => ''], 404);
+});
 
