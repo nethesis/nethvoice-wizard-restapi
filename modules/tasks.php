a4053d0db4766fd46cff2e0e1112b98b073063dc
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-11-23 10:36:49 +0100

    Add long running processes

---
+++ modules/tasks.php
@@ -0,0 +1,30 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once("lib/SystemTasks.php");
+
+# get the status of a running task
+
+$app->get('/tasks/{task}', function (Request $request, Response $response, $args) {
+    $taskId = $request->getAttribute('task');
+    $ret = [ "task" => $taskId ];
+    $code = 200;
+    $st = new SystemTasks();
+    $task = $st->getTaskStatus($taskId);
+    $ret['action'] = $task['last']['title'];
+    $ret['progess'] = ceil($task['progress'] * 100);
+
+    # task has reached di end, set progress to 100
+    if (isset($task['task_command_line'])) {
+        $ret['progess'] = 100;
+
+        # return server error if task has failed
+        if ($task['exit_code'] > 0) {
+            $code = 500;
+        }
+    }
+    
+    return $response->withJson($ret, $code);
+});
+
