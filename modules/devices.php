d8deb3f57d9c145654140b9986bd07048e563924
Stefano Fancello <stefano.fancello@nethesis.it>
2016-11-30 18:11:49 +0100

    Add network scan API. POST /devices/scan. Refs task #295

---
+++ modules/devices.php
@@ -0,0 +1,22 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once("lib/SystemTasks.php");
+
+
+/*
+*  Launch a scan: POST/devices/scan
+*  Parameter: { "network": "192.168.0.0/24"}
+*/
+
+$app->post('/devices/scan', function (Request $request, Response $response, $args) {
+    $params = $request->getParsedBody();
+    $network = $params['network'];
+
+    //launch long running process
+    $st = new SystemTasks();
+    $taskId = $st->startTask("/usr/bin/sudo /var/www/html/freepbx/rest/lib/scanHelper.py ".escapeshellarg($network));
+    return $response->withJson($taskId, 200);
+});
+
