797ea8970b4fa81d23366b0fc59ad900d2116c7f
Alessandro Polidori <alessandro.polidori@nethesis.it>
2016-12-01 10:22:54 +0100

    add try/catch and doc. task #315

--- modules/trunks.php
+++ modules/trunks.php
@@ -2,27 +2,43 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-# returns all trunks
+/**
+ * @api {get} /trunks  Retrieve all trunks
+ */
 $app->get('/trunks', function (Request $request, Response $response, $args) {
-    $result = array();
-    $trunks = FreePBX::Core()->listTrunks();
-    foreach($trunks as $trunk) {
-        array_push($result, $trunk);
-    }
-    return $response->withJson($result,200);
-});
-
-# returns trunks by tech
-$app->get('/trunks/{tech}', function (Request $request, Response $response, $args) {
-    $result = array();
-    $trunks = FreePBX::Core()->listTrunks();
-    $tech = $request->getAttribute('tech');
-    $tech = strtolower($tech);
-
-    foreach($trunks as $trunk) {
-        if (strtolower($trunk['tech']) == $tech) {
+    try {
+        $result = array();
+        $trunks = FreePBX::Core()->listTrunks();
+        foreach($trunks as $trunk) {
             array_push($result, $trunk);
         }
+        return $response->withJson($result,200);
     }
-    return $response->withJson($result);
+    catch (Exception $e) {
+      error_log($e->getMessage());
+      return $response->withJson('An error occurred', 500);
+    }
+});
+
+/**
+ * @api {get} /trunks/{tech}  Retrieve all trunks by technology
+ */
+$app->get('/trunks/{tech}', function (Request $request, Response $response, $args) {
+    try {
+        $result = array();
+        $trunks = FreePBX::Core()->listTrunks();
+        $tech = $request->getAttribute('tech');
+        $tech = strtolower($tech);
+
+        foreach($trunks as $trunk) {
+            if (strtolower($trunk['tech']) == $tech) {
+                array_push($result, $trunk);
+            }
+        }
+        return $response->withJson($result);
+    }
+    catch (Exception $e) {
+      error_log($e->getMessage());
+      return $response->withJson('An error occurred', 500);
+    }
 });
