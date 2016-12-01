7cb21cae8c20d25afdc0798e28f430edd54c7d7b
Alessandro Polidori <alessandro.polidori@nethesis.it>
2016-12-01 12:12:17 +0100

    new API GET /providers. task #320

---
+++ modules/providers.php
@@ -0,0 +1,24 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once('/var/www/html/freepbx/admin/modules/core/functions.inc.php');
+
+/**
+ * @api {get} /providers  Retrieve all providers
+ */
+$app->get('/providers', function (Request $request, Response $response, $args) {
+    try {
+        global $db;
+        $sql = 'SELECT * FROM `providers`';
+        $results = $db->getAll($sql, DB_FETCHMODE_ASSOC);
+        if(DB::IsError($results)) {
+            throw new Exception($results->getMessage());
+        }
+        return $response->withJson($results, 200);
+    }
+    catch (Exception $e) {
+        error_log($e->getMessage());
+        return $response->withJson('An error occurred', 500);
+    }
+});
