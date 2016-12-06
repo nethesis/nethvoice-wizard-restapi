c6a8ba847dee8d765a75e1719879b37bfa571a65
Stefano Fancello <stefano.fancello@nethesis.it>
2016-12-06 16:36:24 +0100

    Add primary key to table rest_mobiles. Refs task #278

--- sql/mobiles.sql
+++ sql/mobiles.sql
@@ -1,6 +1,7 @@
 USE asterisk;
 DROP TABLE IF EXISTS `rest_mobiles`;
 CREATE TABLE IF NOT EXISTS `rest_mobiles`(
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `mobile` varchar(20), 
   `username` varchar(150) DEFAULT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
