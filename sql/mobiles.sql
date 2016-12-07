4863c72a65d558e9c664b3ef5539bd66eed5e42b
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2016-12-07 10:34:33 +0100

    Fix rest_mobiles table. Task #278

--- sql/mobiles.sql
+++ sql/mobiles.sql
@@ -1,9 +1,8 @@
 USE asterisk;
 DROP TABLE IF EXISTS `rest_mobiles`;
 CREATE TABLE IF NOT EXISTS `rest_mobiles`(
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `mobile` varchar(20), 
-  `username` varchar(150) DEFAULT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
+  `username` varchar(150) DEFAULT NULL,
+  KEY `username_key` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
