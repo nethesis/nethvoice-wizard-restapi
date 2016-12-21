daf1467f69b6f3f5f65179ec4592ea73caf82f29
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2016-12-21 10:23:57 +0100

    Fix GET /codecs/voip API

--- modules/codecs.php
+++ modules/codecs.php
@@ -7,24 +7,21 @@ use \Psr\Http\Message\ResponseInterface as Response;
 */
 
 $app->get('/codecs/voip', function (Request $request, Response $response, $args) {
+    $codecs = array(
+        array ('codec' => 'alaw', 'enabled' => true),
+        array ('codec' => 'ulaw', 'enabled' => true)
+    );
+
     try{
         //check if G729 is installed
         exec('/usr/sbin/asterisk -rx "module show like codec_g729.so"', $out, $ret);
-        if ($ret === 0 && strpos("codec_g729.so",implode($out)) !== false ) {
+        if ($ret === 0 && strpos(implode($out), 'codec_g729.so') !== false) {
             //codec g729 found
-            $out = array(
-                array ('codec' => 'g729', 'enabled' => true),
-                array ('codec' => 'alaw', 'enabled' => false),
-                array ('codec' => 'ulaw', 'enabled' => false)
-              );
-            return $response->withJson($out, 200);
-        } else {
-            $out = array(
-                array ('codec' => 'alaw', 'enabled' => true),
-                array ('codec' => 'ulaw', 'enabled' => true)
-              );
-            return $response->withJson($out, 200);
+            $codecs = array_map(function($a) { $a['enabled'] = false; return $a; }, $codecs);
+            $codecs[] = array('codec' => 'g729', 'enabled' => true);
         }
+
+        return $response->withJson($codecs, 200);
     } catch (Exception $e) {
         error_log($e->getMessage());
         return $response->withJson('An error occurred', 500);
