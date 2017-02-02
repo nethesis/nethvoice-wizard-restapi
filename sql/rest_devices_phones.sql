61b64c1aae94d9772613d0a6cfe878bb65f29199
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2017-02-02 11:58:41 +0100

    Fix rest_devices_phones table sql.

--- sql/rest_devices_phones.sql
+++ sql/rest_devices_phones.sql
@@ -4,8 +4,9 @@ CREATE TABLE IF NOT EXISTS `rest_devices_phones`(
   `mac` varchar(20),
   `vendor` varchar(64) DEFAULT NULL,
   `model` varchar(64) DEFAULT NULL,
-  `line` int DEFAULT NULL,
+  `line` int DEFAULT '1',
   `mainextension` varchar(16) DEFAULT NULL,
   `extension` varchar(16) DEFAULT NULL,
-  `secret` varchar(128) DEFAULT NULL
+  `secret` varchar(128) DEFAULT NULL,
+  UNIQUE KEY `mac` (`mac`,`line`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
