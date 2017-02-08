41713f25fcb2a909ad8fb796a75b81ca664c99f7
Stefano Fancello <stefano.fancello@nethesis.it>
2017-02-08 09:18:30 +0100

    CTI permissions db structure

---
+++ sql/rest_cti.sql
@@ -0,0 +1,51 @@
+USE asterisk;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_profiles`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL DEFAULT 'Custom'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_macro_permissions`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_permissions`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(1024) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_profiles_permissions`(
+  `profile_id` INT UNSIGNED NOT NULL,
+  `permission_id` INT UNSIGNED NOT NULL,
+  FOREIGN KEY (`profile_id`) REFERENCES `rest_cti_profiles`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (`permission_id`) REFERENCES `rest_cti_permissions`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_profiles_macro_permissions`(
+  `profile_id` INT UNSIGNED NOT NULL,
+  `macro_permission_id` INT UNSIGNED NOT NULL,
+  FOREIGN KEY (`profile_id`) REFERENCES `rest_cti_profiles`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (`macro_permission_id`) REFERENCES `rest_cti_macro_permissions`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `rest_cti_macro_permissions_permissions`(
+  `macro_permission_id` INT UNSIGNED NOT NULL,
+  `permission_id` INT UNSIGNED NOT NULL,
+  FOREIGN KEY (`macro_permission_id`) REFERENCES `rest_cti_macro_permissions`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (`permission_id`) REFERENCES `rest_cti_permissions`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
