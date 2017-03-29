5a89d731a037ab686addbb7a4561269120239628
Stefano Fancello <stefano.fancello@nethesis.it>
2017-03-29 11:50:55 +0200

    Use mainextension as first extension assigned to devices

---
+++ lib/libExtensions.php
@@ -0,0 +1,271 @@
+<?php
+
+function extensionExists($e, $extensions)
+{
+    foreach ($extensions as $extension) {
+        if ($extension['extension'] == $e) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function createExtension($mainextensionnumber){
+    try {
+        $fpbx = FreePBX::create();
+        $dbh = FreePBX::Database();
+        $stmt = $dbh->prepare("SELECT * FROM `rest_devices_phones` WHERE `extension` = ?");
+        $stmt->execute(array($mainextensionnumber));
+        $res = $stmt->fetchAll();
+        if (count($res)===0) {
+           //Main extension isn't already used use mainextension as extension
+           return $mainextensionnumber;
+        } else {
+            //create new extension
+            $mainextensions = $fpbx->Core->getAllUsers();
+            foreach ($mainextensions as $ve) {
+                if ($ve['extension'] == $mainextensionnumber) {
+                    $mainextension = $ve;
+                    break;
+                }
+            }
+            //get first free physical extension number for this main extension
+            $extensions = $fpbx->Core->getAllUsersByDeviceType();
+            for ($i=91; $i<=98; $i++) {
+                if (!extensionExists($i.$mainextensionnumber, $extensions)) {
+                    $extension = $i.$mainextensionnumber;
+                    break;
+                }
+            }
+            //error if there aren't available extension numbers
+            if (!isset($extension)) {
+                throw ("There aren't available extension numbers");
+            }
+            //delete extension
+            $fpbx->Core->delDevice($extension, true);
+            $fpbx->Core->delUser($extension, true);
+            //create physical extension
+            $data['name'] = $mainextension['name'];
+            $mainextdata = $fpbx->Core->getUser($mainextension['extension']);
+            $data['outboundcid'] = $mainextdata['outboundcid'];
+            $res = $fpbx->Core->processQuickCreate('pjsip', $extension, $data);
+            if (!$res['status']) {
+                throw ("Error creating extension");
+            }
+            //Add device to main extension devices
+            global $astman;
+            $existingdevices = $astman->database_get("AMPUSER", $mainextensionnumber."/device");
+            if (empty($existingdevices)) {
+                $astman->database_put("AMPUSER", $mainextensionnumber."/device", $extension);
+            } else {
+                $existingdevices_array = explode('&', $existingdevices);
+                if (!in_array($extension, $existingdevices_array)) {
+                    $existingdevices_array[]=$extension;
+                    $existingdevices = implode('&', $existingdevices_array);
+                    $astman->database_put("AMPUSER", $mainextensionnumber."/device", $existingdevices);
+                }
+            }
+        }
+        return $extension;
+    } catch (Exception $e) {
+       error_log($e->getMessage());
+       return false;
+    }
+}
+
+function useExtensionAsWebRTC($extension) {
+    try {
+        // insert WebRTC extension in password table
+        $extension_secret = sql('SELECT data FROM `sip` WHERE id = "' . $extension . '" AND keyword="secret"', "getOne");
+        $dbh = FreePBX::Database();
+        $sql = 'SELECT id FROM rest_devices_phones WHERE extension = ?';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($extension));
+        $res = $stmt->fetchAll();
+        $uidquery = 'SELECT userman_users.id'.
+                ' FROM userman_users'.
+                ' WHERE userman_users.default_extension = ? LIMIT 1';
+        if (empty($res)) {
+            $sql = 'INSERT INTO `rest_devices_phones`'.
+                ' SET user_id = ('. $uidquery. '), extension = ?, secret= ?, type = "webrtc", mac = NULL, line = NULL';
+            $stmt = $dbh->prepare($sql);
+
+            if ($stmt->execute(array(getMainExtension($extension),$extension,$extension_secret))) {
+                return true;
+            }
+        } else {
+            $sql = 'UPDATE `rest_devices_phones`'. 
+                ' SET user_id = ('. $uidquery. '), secret= ?, type = "webrtc"' .
+                ' WHERE extension = ?';
+            if ($stmt->execute(array(getMainExtension($extension),$extension_secret,$extension))) {
+                return true;
+            }
+        } 
+    } catch (Exception $e) {
+       error_log($e->getMessage());
+       return false;
+    }
+}
+
+function useExtensionAsPhysical($extension,$mac,$model,$line=false) {
+    try {
+       // insert created physical extension in password table
+       $extension_secret = sql('SELECT data FROM `sip` WHERE id = "' . $extension . '" AND keyword="secret"', "getOne");
+       $dbh = FreePBX::Database();
+       if ( isset($line) && $line ) {
+           $sql = 'UPDATE `rest_devices_phones` SET user_id = ( '.
+                  'SELECT userman_users.id FROM userman_users WHERE userman_users.default_extension = ? '.
+                  '), extension = ?, secret= ?, type = "physical" WHERE mac = ? AND line = ?';
+           $stmt = $dbh->prepare($sql);
+           $res = $stmt->execute(array(getMainExtension($extension),$extension,$extension_secret,$mac,$line));
+       } else {
+           $sql = 'UPDATE `rest_devices_phones` SET user_id = ( '.
+                  'SELECT userman_users.id FROM userman_users WHERE userman_users.default_extension = ? '.
+                  '), extension = ?, secret= ?, type = "physical" WHERE mac = ?';
+           $stmt = $dbh->prepare($sql);
+           $res = $stmt->execute(array(getMainExtension($extension),$extension,$extension_secret,$mac));
+       }
+       $stmt = $dbh->prepare($sql);
+       if ($res) {
+           // Add extension to endpointman
+           $endpoint = new endpointmanager();
+           // Get model id by mac
+           $brand = $endpoint->get_brand_from_mac($mac);
+           $models = $endpoint->models_available(null, $brand['id']);
+           $model_id = null;
+           foreach ($models as $m) {
+               if ($m['text'] === $model) {
+                   $model_id = $m['value'];
+                   break;
+               }
+           }
+           if (!$model_id) {
+               throw new Exception('model not found');
+           } else {
+               $mac_id = $dbh->sql('SELECT id FROM endpointman_mac_list WHERE mac = "'.preg_replace('/:/', '', $mac).'"', "getOne");
+               if ($mac_id) {
+                    // add line if device already exist
+                   $endpoint->add_line($mac_id, $line, $extension, $mainextension['name']);
+               } else {
+                   // add device to endpointman module
+                   $mac_id = $endpoint->add_device($mac, $model_id, $extension, null, $line, $mainextension['name']);
+               }
+           }
+       } else {
+           throw new Exception("Error adding device");
+       }
+       return true;
+    } catch (Exception $e) {
+       error_log($e->getMessage());
+       return false;
+   }
+}
+
+function isMainExtension($extension) {
+    try {
+        if ($extension == "") {
+            throw new Exception("Error: empty extension");
+        }
+        $dbh = FreePBX::Database();
+        $sql = 'SELECT `username` FROM `userman_users` WHERE `default_extension` = ?';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($extension));
+        $res = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if (isset($res) && !empty($res)) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        return -1;
+    }
+}
+
+function getMainExtension($extension) {
+    try {
+        if (isMainExtension($extension)) {
+            return $extension;
+        } else {
+            return substr($extension, 2);
+        }
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        return -1;
+    }
+}
+
+
+function deleteExtension($extension) {
+    try {
+        global $astman;
+        $dbh = FreePBX::Database();
+        if (isMainExtension($extension) === false) {
+            $mainextensions = substr($extension, 2);
+            // clean extension
+            $fpbx = FreePBX::create();
+            $fpbx->Core->delUser($extension);
+            $fpbx->Core->delDevice($extension);
+
+            //Remove device from main extension
+            $existingdevices = $astman->database_get("AMPUSER", $mainextension."/device");
+            if (!empty($existingdevices)) {
+                $existingdevices_array = explode('&', $existingdevices);
+                unset($existingdevices_array[$extension]);
+                $existingdevices = implode('&', $existingdevices_array);
+                $astman->database_put("AMPUSER", $mainextension."/device", $existingdevices);
+            }
+        }
+
+        $sql = 'UPDATE rest_devices_phones SET user_id = NULL, extension = NULL, secret = NULL, type = NULL WHERE extension = ?';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($extension));
+        $sql = 'DELETE FROM `rest_devices_phones` WHERE user_id IS NULL AND mac IS NULL';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array());
+        return true;
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        return false;
+    }
+}
+
+function deletePhysicalExtension($extension) {
+    try {
+        global $astman;
+        $dbh = FreePBX::Database();
+        //Get device lines
+        $mac = $dbh->sql('SELECT `mac` FROM `rest_devices_phones` WHERE `extension` = "'.$extension.'"', "getOne");
+        $usedlinecount = $dbh->sql('SELECT COUNT(*) FROM `rest_devices_phones` WHERE `mac` = "'.$mac.'" AND `extension` != "" AND `extension`', "getOne");
+
+        // Remove endpoint from endpointman
+        $endpoint = new endpointmanager();
+        $mac_id = $dbh->sql('SELECT id FROM endpointman_mac_list WHERE mac = "'.preg_replace('/:/', '', $mac).'"', "getOne");
+        if (!empty($mac_id)) {
+            $luid = $dbh->sql('SELECT luid FROM endpointman_line_list WHERE mac_id = "'.$mac_id.'" AND ext = "'.$extension.'"', "getOne");
+            if ($usedlinecount > 1) {
+                //There are other configured lines for this device
+                $endpoint->delete_line($luid, false);
+            } else {
+                //last line, also remove device
+                $endpoint->delete_line($luid, true);
+            }
+        }
+        return true;
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        return false;
+    }
+}
+
+function getWebRTCExtension($mainextension) {
+    $dbh = FreePBX::Database();
+    $uidquery = 'SELECT userman_users.id'.
+       ' FROM userman_users'.
+       ' WHERE userman_users.default_extension = ?';
+    $sql = 'SELECT extension FROM `rest_devices_phones` WHERE user_id = ('. $uidquery. ') AND type = "webrtc"';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute(array($mainextension));
+    return $stmt->fetchAll()[0][0];
+}
+
--- modules/physicalextensions.php
+++ modules/physicalextensions.php
@@ -6,15 +6,7 @@ require_once(__DIR__. '/../lib/freepbxFwConsole.php');
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-function extensionExists($e, $extensions)
-{
-    foreach ($extensions as $extension) {
-        if ($extension['extension'] == $e) {
-            return true;
-        }
-    }
-    return false;
-}
+include_once('lib/libExtensions.php');
 
 $app->get('/physicalextensions', function (Request $request, Response $response, $args) {
     $physicalextensions = FreePBX::create()->Core->getAllUsersByDeviceType('pjsip');
@@ -40,120 +32,23 @@ $app->post('/physicalextensions', function (Request $request, Response $response
         $mac = $params['mac'];
         $model = $params['model'];
         $line = $params['line'];
-        $fpbx = FreePBX::create();
 
-      //get associated main extension
-      $mainextensions = $fpbx->Core->getAllUsers();
-        foreach ($mainextensions as $ve) {
-            if ($ve['extension'] == $mainextensionnumber) {
-                $mainextension = $ve;
-                break;
-            }
-        }
-      //error if main extension number doesn't exist
-      if (!isset($mainextension)) {
-          return $response->withJson(array("status"=>"Main extension ".$mainextensionnumber." doesn't exist"), 400);
-      }
+        $extension = createExtension($mainextensionnumber);
 
-        if (isset($params['extension'])) {
-            //use given extension number
-         if (!preg_match('/9[1-7]'.$mainextensionnumber.'/', $params['extension'])) {
-             return $response->withJson(array("status"=>"Wrong physical extension number supplied"), 400);
-         } else {
-             $extension = $params['extension'];
-         }
-        } else {
-            //get first free physical extension number for this main extension
-          $extensions = $fpbx->Core->getAllUsersByDeviceType();
-            for ($i=91; $i<=97; $i++) {
-                if (!extensionExists($i.$mainextensionnumber, $extensions)) {
-                    $extension = $i.$mainextensionnumber;
-                    break;
-                }
-            }
-          //error if there aren't available extension numbers
-          if (!isset($extension)) {
-              return $response->withJson(array("status"=>"There aren't available extension numbers"), 500);
-          }
+        if ($extension === false ) {
+            $response->withJson(array("status"=>"Error creating extension"), 500);
         }
 
-      //delete extension
-      $fpbx->Core->delDevice($extension, true);
-        $fpbx->Core->delUser($extension, true);
-
-      //create physical extension
-      $data['name'] = $mainextension['name'];
-        $mainextdata = $fpbx->Core->getUser($mainextension['extension']);
-        $data['outboundcid'] = $mainextdata['outboundcid'];
-        $res = $fpbx->Core->processQuickCreate('pjsip', $extension, $data);
-        if (!$res['status']) {
-            return $response->withJson(array('message'=>$res['message']), 500);
+        if (useExtensionAsPhysical($extension,$mac,$model,$line) === false) {
+            $response->withJson(array("status"=>"Error associating physical extension"), 500);
         }
 
-      //Add device to main extension
-      global $astman;
-        $existingdevices = $astman->database_get("AMPUSER", $mainextensionnumber."/device");
-        if (empty($existingdevices)) {
-            $astman->database_put("AMPUSER", $mainextensionnumber."/device", $extension);
-        } else {
-            $existingdevices_array = explode('&', $existingdevices);
-            if (!in_array($extension, $existingdevices_array)) {
-                $existingdevices_array[]=$extension;
-                $existingdevices = implode('&', $existingdevices_array);
-                $astman->database_put("AMPUSER", $mainextensionnumber."/device", $existingdevices);
-            }
-        }
-      // insert created physical extension in password table
-      $created_extension = $res['ext'];
-        $created_extension_secret = sql('SELECT data FROM `sip` WHERE id = "' . $created_extension . '" AND keyword="secret"', "getOne");
-        $dbh = FreePBX::Database();
-        $subquery = 'SELECT userman_users.id'.
-          ' FROM userman_users'.
-          ' WHERE userman_users.default_extension = ?';
-
-        $sql = 'UPDATE `rest_devices_phones`'.
-          ' SET user_id = ('. $subquery. '), extension = ?, secret= ?'.
-          ' WHERE mac = \''. $mac. '\''.
-          ((isset($line) && $line) ? ' AND line = '. $line : '');
-        $stmt = $dbh->prepare($sql);
-        if ($res = $stmt->execute(array($mainextensionnumber,$created_extension,$created_extension_secret))) {
-            // Add extension to endpointman
-          $endpoint = new endpointmanager();
-
-          // Get model id by mac
-          $brand = $endpoint->get_brand_from_mac($mac);
-            $models = $endpoint->models_available(null, $brand['id']);
-
-            $model_id = null;
-            foreach ($models as $m) {
-                if ($m['text'] === $model) {
-                    $model_id = $m['value'];
-                    break;
-                }
-            }
-
-            if (!$model_id) {
-                throw new Exception('model not found');
-            } else {
-                $mac_id = $dbh->sql('SELECT id FROM endpointman_mac_list WHERE mac = "'.preg_replace('/:/', '', $mac).'"', "getOne");
-                if ($mac_id) {
-                    // add line if device already exist
-                  $endpoint->add_line($mac_id, $line, $created_extension, $mainextension['name']);
-                } else {
-                    // add device to endpointman module
-                  $mac_id = $endpoint->add_device($mac, $model_id, $created_extension, null, $line, $mainextension['name']);
-                }
-            }
-
-            fwconsole('r');
-            return $response->withJson(array('extension'=>$created_extension), 200);
-        } else {
-            throw new Exception();
-        }
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-        return $response->withStatus(500);
-    }
+        fwconsole('r');
+        return $response->withJson(array('extension'=>$created_extension), 200);
+   } catch (Exception $e) {
+       error_log($e->getMessage());
+       return $response->withJson(array("status"=>$e->getMessage()), 500);
+   }
 });
 
 $app->delete('/physicalextensions/{extension}', function (Request $request, Response $response, $args) {
@@ -161,46 +56,13 @@ $app->delete('/physicalextensions/{extension}', function (Request $request, Resp
         global $astman;
         $route = $request->getAttribute('route');
         $extension = $route->getArgument('extension');
-        $mainextensions = substr($extension, 2);
-        $dbh = FreePBX::Database();
 
-        //Get device lines
-        $mac = $dbh->sql('SELECT `mac` FROM `rest_devices_phones` WHERE `extension` = "'.$extension.'"', "getOne");
-        $usedlinecount = $dbh->sql('SELECT COUNT(*) FROM `rest_devices_phones` WHERE `mac` = "'.$mac.'" AND `extension` != "" AND `extension`', "getOne");
-
-        // clean extension
-        $fpbx = FreePBX::create();
-        $fpbx->Core->delUser($extension);
-        $fpbx->Core->delDevice($extension);
-        $sql = 'UPDATE rest_devices_phones SET user_id = NULL, extension = NULL, secret = NULL WHERE extension = ?';
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute(array($extension));
-
-        //Remove device from main extension
-        $existingdevices = $astman->database_get("AMPUSER", $mainextension."/device");
-        if (!empty($existingdevices)) {
-            $existingdevices_array = explode('&', $existingdevices);
-            unset($existingdevices_array[$extension]);
-            $existingdevices = implode('&', $existingdevices_array);
-            $astman->database_put("AMPUSER", $mainextension."/device", $existingdevices);
-        }
-
-
-        // Remove endpoint from endpointman
-        $endpoint = new endpointmanager();
-        $mac_id = $dbh->sql('SELECT id FROM endpointman_mac_list WHERE mac = "'.preg_replace('/:/', '', $mac).'"', "getOne");
-        $luid = $dbh->sql('SELECT luid FROM endpointman_line_list WHERE mac_id = "'.$mac_id.'" AND ext = "'.$extension.'"', "getOne");
-
-        if ($usedlinecount > 1) {
-            //There are other configured lines for this device
-            $endpoint->delete_line($luid, false);
+        if (deletePhysicalExtension($extension) && deleteExtension($extension)) {
+            fwconsole('r');
+            return $response->withStatus(200);
         } else {
-            //last line, also remove device
-            $endpoint->delete_line($luid, true);
+            throw new Exception("Error deleting extension");
         }
-
-        fwconsole('r');
-        return $response->withStatus(200);
     } catch (Exception $e) {
         error_log($e->getMessage());
         return $response->withStatus(500);
