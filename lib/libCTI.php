6fe58e87171f862c28f58a05dc446c3c135237bd
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-05-04 17:34:23 +0200

    fix newline for long query

--- lib/libCTI.php
+++ lib/libCTI.php
@@ -52,7 +52,10 @@ function getAllAvailableMacroPermissionsPermissions() {
 function getCTIGroups() {
     try {
         $dbh = FreePBX::Database();
-        $sql = 'select uu.username, cg.name from rest_cti_users_groups ug join rest_cti_groups cg on ug.group_id = cg.id join rest_users ru on ru.id = ug.user_id join userman_users uu on uu.id = ru.user_id;';
+        $sql = 'SELECT uu.username, cg.name FROM rest_cti_users_groups ug'.
+               ' JOIN rest_cti_groups cg ON ug.group_id = cg.id'.
+               ' JOIN rest_users ru ON ru.id = ug.user_id'.
+               ' JOIN userman_users uu on uu.id = ru.user_id;';
         $result = $dbh->sql($sql,"getAll",\PDO::FETCH_ASSOC);
         return $result;
     } catch (Exception $e) {
