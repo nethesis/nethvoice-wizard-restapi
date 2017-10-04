b8aee3d7a8ac5472dad335fd2769d615b6131286
Stefano Fancello <stefano.fancello@nethesis.it>
2017-10-04 12:12:12 +0200

    Change mac scan app login. Use FreePBX class instead of curl

--- modules/authentication.php
+++ modules/authentication.php
@@ -35,29 +35,25 @@ $app->get('/login', function (Request $request, Response $response) {
     return $response->withJson(['success' => true]);
 });
 
-$app->post('/testauth', function (Request $request, Response $response, $args) { //TODO: ??
+$app->post('/testauth', function (Request $request, Response $response, $args) { 
     $params = $request->getParsedBody();
     $username = $params['username'];
     $password = $params['password'];
 
-    $url = "https://".$_SERVER['SERVER_NAME']."/freepbx/admin/config.php?" .
-        "username=" . $username . "&password=" . $password;
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $output = curl_exec($ch);
-    preg_match("/<li>Invalid/",$output, $matches);
-    curl_close($ch);
-
-    if(count($matches) > 0) {
-        return $response->withJson(['result' => 'not authorized'], 401);
-    } else {
-        require(__DIR__.'/../config.inc.php');
-        $secret = $config['settings']['secretkey'];
-        return $response->withJson(['result' => $secret], 200);
+    if(!empty($username)) {
+        // not logged in, and have provided a user/pass
+        include_once(__DIR__.'/../../admin/libraries/ampuser.class.php');
+        session_start();
+        $_SESSION['AMP_user'] = new ampuser($username);
+        if ($_SESSION['AMP_user']->checkPassword($password) && $_SESSION['AMP_user']->checkSection('*')) {
+            unset($no_auth);
+            require(__DIR__.'/../config.inc.php');
+            $secret = $config['settings']['secretkey'];
+            return $response->withJson(['result' => $secret], 200);
+        }
     }
+    unset($_SESSION['AMP_user']);
+    $no_auth = true;
+    return $response->withCode(401);
 });
 
