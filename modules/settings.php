682bf5d13fbea1706843c7a3cc2f6ab12a0aeb8e
Stefano Fancello <stefano.fancello@nethesis.it>
2017-10-06 12:29:47 +0200

    Set tonezone with language

--- modules/settings.php
+++ modules/settings.php
@@ -49,9 +49,20 @@ $app->post('/settings/language', function (Request $request, Response $response,
 */
 $app->post('/settings/defaultlanguage', function (Request $request, Response $response, $args) {
     try {
+        global $amp_conf;
         $data = $request->getParsedBody();
         $lang = $data['lang'];
         FreePBX::create()->Soundlang->setLanguage($lang);
+        # Set tonescheme
+        switch ($lang) {
+            case 'en':
+                $tonescheme = 'us';
+            break;
+            default:
+                $tonescheme = $lang;
+            break;
+        }
+        FreePBX::create()->Core->config->set_conf_values(array('TONEZONE'=>$tonescheme),true,$amp_conf['AS_OVERRIDE_READONLY']);
         system('/var/www/html/freepbx/rest/lib/retrieveHelper.sh > /dev/null &');
     } catch (Exception $e) {
         error_log($e->getMessage());
