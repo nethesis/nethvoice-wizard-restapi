4d45a3a8e7be73d8821030ed90ae7a54eeeb063b
Stefano Fancello <stefano.fancello@nethesis.it>
2019-01-21 16:38:55 +0100

    Only show unknown devices as GS Wave devices

--- root/var/www/html/freepbx/rest/modules/devices.php
+++ root/var/www/html/freepbx/rest/modules/devices.php
@@ -150,6 +150,8 @@ $app->get('/devices/phones/list/{id}', function (Request $request, Response $res
         // Scan messages for GS Wave devices
         $cmd = '/usr/bin/sudo /usr/bin/grep dnsmasq-tftp /var/log/messages ';
         $cmd .= ' | grep \'\/var\/lib\/tftpboot\/cfg[0-9a-fA-F]\{12\}\.xml not found\' ';
+        // take only UNKNOWN MACs
+        $cmd .= " | grep -v '$matchString' ";
         // get only MAC addresses of string
         $cmd .= ' | sed \'s/^.*tftpboot\/cfg\([0-9a-fA-F]\{12\}\)\.xml.*$/\1/\' ';
         // MAC to uppercase
