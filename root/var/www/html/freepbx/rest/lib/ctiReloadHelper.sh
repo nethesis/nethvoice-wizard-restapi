e22207d9cef078155d6c18e411d1309f98ff8986
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-01-14 10:14:25 +0100

    cti: expand config before restart
    If the nethcti-server package is installed before nethvoice,
    the nethcti.json file will not contain flexisip port.
    As consequence, applications configured using the QR code from
    the CTI will not recieve proxy configuration.

--- root/var/www/html/freepbx/rest/lib/ctiReloadHelper.sh
+++ root/var/www/html/freepbx/rest/lib/ctiReloadHelper.sh
@@ -3,5 +3,7 @@ while (ps aux | grep -q [r]etrieve_conf); do
     sleep 1
 done
 sleep 2
+# make sure flexisip configuration is present before reload
+/sbin/e-smith/expand-template /etc/nethcti/nethcti.json
 /usr/bin/sudo /usr/bin/systemctl reload nethcti-server
 [[ -d /opt/nethvoice-report/api/ ]] && /usr/bin/sudo /usr/bin/systemctl restart nethvoice-report-api
