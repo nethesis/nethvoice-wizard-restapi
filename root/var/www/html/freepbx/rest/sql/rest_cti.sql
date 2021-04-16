7f86f5770babe278f580595e4ef9bd56ceeebc32
Stefano Fancello <stefano.fancello@nethesis.it>
2021-04-16 18:03:51 +0200

    Add group CDR permission

--- root/var/www/html/freepbx/rest/sql/rest_cti.sql
+++ root/var/www/html/freepbx/rest/sql/rest_cti.sql
@@ -137,6 +137,7 @@ INSERT IGNORE INTO `rest_cti_permissions` VALUES (27,'ad_off_hour','Admin Off Ho
 INSERT IGNORE INTO `rest_cti_permissions` VALUES (1000,'screen_sharing','Screen Sharing','Allow to share the desktop');
 INSERT IGNORE INTO `rest_cti_permissions` VALUES (2000,'phone_buttons','Phone buttons','Allow to customize physical phone buttons');
 INSERT IGNORE INTO `rest_cti_permissions` VALUES (3000,'video_conference','Video Conference','Allow to start a video conference');
+INSERT IGNORE INTO `rest_cti_permissions` VALUES (4000,'group_cdr','Group CDR','Allow to see call history of members of user groups');
 
 /*Permission inside macro permissions*/
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (1,2);
@@ -149,6 +150,7 @@ INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (1,9);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (1,1000);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (1,2000);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (1,3000);
+INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (3,4000);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (2,12);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (3,13);
 INSERT IGNORE INTO `rest_cti_macro_permissions_permissions` VALUES (5,15);
