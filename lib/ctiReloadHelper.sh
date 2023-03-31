afc1172bbb74a562c831a719830c5c631b0ce451
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2023-03-31 16:18:02 +0200

    cti helper: use service notifier

--- lib/ctiReloadHelper.sh
+++ lib/ctiReloadHelper.sh
@@ -3,5 +3,5 @@ while (ps aux | grep -q [r]etrieve_conf); do
     sleep 1
 done
 sleep 2
-/usr/bin/sudo /usr/bin/systemctl reload nethcti-server
-[[ -d /opt/nethvoice-report/api/ ]] && /usr/bin/sudo /usr/bin/systemctl restart nethvoice-report-api
+
+touch /notify
